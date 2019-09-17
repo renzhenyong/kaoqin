62545aa775ce71ca8231bcde1c6f125a14b57e7e
renzhenyong <renzhenyong@chxgk.com>
2019-09-17 17:41:40 +0800

    考勤首頁

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,117 +1,34 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    scale: 13,
     latitude: "",
     longitude: "",
-    markers: [{
-      iconPath: "/resources/others.png",
-      id: 0,
-      latitude: 23.099994,
-      longitude: 113.324520,
-      width: 50,
-      height: 50
-    }],
-    polyline: [{
-      points: [{
-        longitude: 113.3245211,
-        latitude: 23.10229
-      }, {
-        longitude: 113.324520,
-        latitude: 23.21229
-      }],
-      color: "#FF0000DD",
-      width: 2,
-      dottedLine: true
-    }],
-    controls: [{
-      id: 1,
-      iconPath: '/img/ok.png',
-      position: {
-        left: 0,
-        top: 300 - 50,
-        width: 50,
-        height: 50
-      },
-      clickable: true
-    }]
+    scale: 16,
+    markers: []
   },
-
-
-
-
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    var _this = this;
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  onLoad: function (options) {
+    var that = this
+    //获取当前的地理位置、速度
     wx.getLocation({
       type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
       success: function (res) {
-
-        _this.setData({
-          latitude: res.latitude,
-          longitude: res.longitude,
-          markers: [{
-            id: "1",
-            latitude: res.latitude,
-            longitude: res.longitude,
-            width: 50,
-            height: 50,
-            iconPath: "/img/ok.png",
-            title: "哪里"
-
-          }],
-          circles: [{
-            latitude: res.latitude,
-            longitude: res.longitude,
-            color: '#FF0000DD',
-            fillColor: '#7cb5ec88',
-            radius: 3000,
-            strokeWidth: 1
-          }]
-
+        console.log(res.latitude);
+        console.log(res.longitude);
+        //赋值经纬度
+        that.setData({
+          latitude: parseFloat(res.latitude),
+          longitude: parseFloat(res.longitude),
         })
       }
-
     })
-
 
   },
   getUserInfo: function(e) {
@@ -129,22 +46,4 @@ Page({
       url: '../login/index'
     })
   },
-  getLocation:function(){
-    wx.getLocation({
-      type: 'wgs84',
-      success(res) {
-        console.log("经度："+res.latitude);
-        console.log("纬度：" + res.longitude);
-        const latitude = res.latitude
-        const longitude = res.longitude
-        const speed = res.speed
-        const accuracy = res.accuracy
-      }
-    })
-  },
-
-
-
-
-
 })
--- pages/login/index.js
+++ pages/login/index.js
@@ -6,7 +6,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: [{
+      text: "对话",
+      iconPath: "/example/images/tabbar_icon_chat_default.png",
+      selectedIconPath: "/example/images/tabbar_icon_chat_active.png",
+    },
+    {
+      text: "设置",
+      iconPath: "/example/images/tabbar_icon_setting_default.png",
+      selectedIconPath: "/example/images/tabbar_icon_setting_active.png",
+      badge: 'New'
+    }]
   },
 
   /**
