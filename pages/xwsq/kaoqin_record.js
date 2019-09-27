7f10d6523a5480351e4dd2fdfa0b76c8fc11ef58
renzhenyong <renzhenyong@chxgk.com>
2019-09-27 17:34:19 +0800

    考勤记录

--- pages/xwsq/kaoqin_record.js
+++ pages/xwsq/kaoqin_record.js
@@ -1,19 +1,56 @@
 // pages/xwsq/kaoqin_record.js
-
+const MONTHS = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'June.', 'July.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    demo4_days_style: [{ month: 'current',color: 'white'}],
+    year: new Date().getFullYear(),      // 年份
+    month: new Date().getMonth() + 1,    // 月份
+    day: new Date().getDate(),
+    str: MONTHS[new Date().getMonth()],  // 月份字符串
+    demo4_days_style: [],
+ 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const days_count = new Date(this.data.year, this.data.month, 0).getDate();
+    console.log("days_count");
+    console.log(this.data.day);
 
+    let demo4_days_style = new Array;
+    for (let i = 1; i <= days_count; i++) {
+      if (i == this.data.day) {
+
+        demo4_days_style.push({
+          month: 'current', day: i, color: '#3F88FB', background: 'white'
+        });
+    
+      } else if (i == 1 || i == 2 || i == 4 || i == 3 || i == 5 || i == 6 || i == 24) {
+        demo4_days_style.push({
+          month: 'current', day: i, color: 'white', background: 'green'
+        });
+      } else if (i == 12 || i == 23 || i == 24) {
+        demo4_days_style.push({
+          month: 'current', day: i, color: 'white', background: 'green'
+        });
+      } else if (i == 21 || i == 22) {
+        demo4_days_style.push({
+          month: 'current', day: i, color: 'white', background: '#eb4986'
+        });
+      } else {
+        demo4_days_style.push({
+          month: 'current', day: i, color: 'white'
+        });
+      }
+    }
+    this.setData({
+      demo4_days_style
+    });
   },
 
   /**
